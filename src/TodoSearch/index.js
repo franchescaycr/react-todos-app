d2ab5fbfcb630b5ae65058e91cbd95cda3c4174c
Franchesca Carrasco <franchescay.carrasco@gmail.com>
2025-04-10 22:46:39 -0300

    cambien placeholder

--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -10,7 +10,7 @@ function TodoSearch() {
   
   return (
     <input
-      placeholder="Cortar cebolla"
+      placeholder="Buscar..."
       className="TodoSearch"
       value={searchValue}
       onChange={(event) => {
